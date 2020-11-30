62f35efc18ae25b2a130158db00e7d9521dfa73a
h92 <h6+github@protonmail.com>
2020-11-30 10:31:34 +0000

    slow point class

--- unrail_nimbers.py
+++ unrail_nimbers.py
@@ -16,17 +16,33 @@ class Point:
         self.y = y
 
     def __lt__(self, other):
-        return self.x < other.x or self.y < self.y
+        return self.x < other.x or (self.x == other.x and self.y < other.y)
 
     def __add__(self, other):
-        return point(self.x+other.x, self.y+other.y)
+        return Point(self.x+other.x, self.y+other.y)
+
+    def __sub__(self, other):
+        return Point(self.x-other.x, self.y-other.y)
 
     def __mul__(self, scalar):
-        return point(self.x*scalar, self.y*scalar)
+        return Point(self.x*scalar, self.y*scalar)
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+    def __repr__(self):
+        return f'({self.x}, {self.y})'
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return tuple((self.x, self.y)).__hash__()
 
 
 def complex_compare(x):
-    return x.real,x.imag
+    return x
+    #return x.real,x.imag
 
 def transform(rail):
     def smaller(x, y):
@@ -38,13 +54,13 @@ def transform(rail):
         return False
 
     transforms = (
-        lambda x: complex(x.real, -x.imag),
-        lambda x: complex(-x.real, x.imag),
-        lambda x: complex(-x.real, -x.imag),
-        lambda x: complex(x.imag, x.real),
-        lambda x: complex(x.imag, -x.real),
-        lambda x: complex(-x.imag, x.real),
-        lambda x: complex(-x.imag, -x.real),
+        lambda x: Point(x.x, -x.y),
+        lambda x: Point(-x.x, x.y),
+        lambda x: Point(-x.x, -x.y),
+        lambda x: Point(x.y, x.x),
+        lambda x: Point(x.y, -x.x),
+        lambda x: Point(-x.y, x.x),
+        lambda x: Point(-x.y, -x.x),
     )
 
     transformed_rails = [[]*7]
@@ -70,12 +86,12 @@ def shift_to_origo(rail):
     min_x = None
     min_y = None
     for piece in rail:
-        if min_x == None or piece.real < min_x:
-            min_x = piece.real
-        if min_y == None or piece.imag < min_y:
-            min_y = piece.imag
+        if min_x == None or piece.x < min_x:
+            min_x = piece.x
+        if min_y == None or piece.y < min_y:
+            min_y = piece.y
 
-    return tuple(point-complex(min_x, min_y) for point in rail)
+    return tuple(point-Point(min_x, min_y) for point in rail)
 
 def smallest_nimber_not_in(nimbers):
     i = 0
@@ -93,8 +109,8 @@ def split(rail):
         while True:
             added = []
             for rem in remaining:
-                for offset in (complex(0,1), complex(1,0),
-                               complex(0, -1), complex(-1, 0)):
+                for offset in (Point(0,1), Point(1,0),
+                               Point(0, -1), Point(-1, 0)):
                     if offset+rem in split_rails[-1]:
                         added.append(rem)
                         break
@@ -134,25 +150,25 @@ def nimber(rail):
     rows=0
     cols=0
     for coord in rail:
-        if coord.real > rows:
-            rows = int(coord.real)
-        if coord.imag > cols:
-            cols = int(coord.imag)
+        if coord.x > rows:
+            rows = coord.x
+        if coord.y > cols:
+            cols = coord.y
 
     global_nimbers = set()
 
     for row in range(rows+1):
         for col in range(cols+1):
-            if complex(row,col) not in rail:
+            if Point(row,col) not in rail:
                 continue
 
-            for mod in (complex(0,1), complex(1,0)):
+            for mod in (Point(0,1), Point(1,0)):
                 for num in (1,2,3):
                     # ugly optimization to not double check single removal
-                    if mod == complex(1,0) and num==1:
+                    if mod == Point(1,0) and num==1:
                         continue
 
-                    remove = [complex(row,col) + mod*i for i in range(num)]
+                    remove = [Point(row,col) + mod*i for i in range(num)]
                     valid = True
 
                     for r in remove:
@@ -204,15 +220,16 @@ if __name__ == '__main__':
     #print( nimber(((0,0),(0,1)) ))
     #print( split(((0,0),(0,1),(2,2)) ))
 
-    k = tuple(complex(x,y) for x in range(4) for y in range(4))
+    k = tuple(Point(x,y) for x in range(4) for y in range(4))
     print(nimber(k))
     print(len(results))
-    #print(results)
+    #import pprint
+    #pprint.pprint(results)
 
-    with open('results.pickle', 'wb') as f:
-        pickle.dump(results, f)
-    with open('optimal_moves.pickle', 'wb') as f:
-        pickle.dump(optimal_moves, f)
+    #with open('results.pickle', 'wb') as f:
+    #    pickle.dump(results, f)
+    #with open('optimal_moves.pickle', 'wb') as f:
+    #    pickle.dump(optimal_moves, f)
 
 
 
