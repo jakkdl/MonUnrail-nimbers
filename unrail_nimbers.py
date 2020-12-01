6ccc73106569c2b1e8155b5dec4d9095d5e2eccb
h92 <h6+github@protonmail.com>
2020-12-01 20:25:15 +0000

    starting to work on transform3

--- unrail_nimbers.py
+++ unrail_nimbers.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
+import argparse
 import os
 import pickle
 
 results = {
     tuple(): 0
 }
+#untransformed_results = {
+#    tuple(): 0
+#}
 optimal_moves = {
     tuple(): []
 }
@@ -16,11 +20,9 @@ def complex_compare(x):
 def transform(rail):
     def smaller(x, y):
         for a,b in zip(x, y):
-            if complex_compare(a) < complex_compare(b):
-            #if a.real < b.real or (a.real == b.real and a.imag < b.imag):
+            if a.real < b.real or (a.real == b.real and a.imag < b.imag):
                 return True
-            if complex_compare(a) > complex_compare(b):
-            #if a.real > b.real or (a.real == b.real and a.imag > b.imag):
+            if a.real > b.real or (a.real == b.real and a.imag > b.imag):
                 return False
         return False
 
@@ -40,18 +42,99 @@ def transform(rail):
                                                  transforms):
             transformed_rail.append(transform_f(point))
 
+    transformed_rails.append(rail)
+
     transformed_rails = [
         shift_to_origo(
             sorted(t, key=complex_compare)
         ) for t in transformed_rails
     ]
 
-    smallest = rail
-    for t in transformed_rails:
+    smallest = transformed_rails[0]
+    for t in transformed_rails[1:]:
         if smaller(t, smallest):
             smallest = t
 
     return smallest
+
+
+class Transform:
+    def __init__(self, origo, prim_delta, sec_delta, prim_len, sec_len):
+        self.origo = origo
+        self.prim_delta = delta
+        self.sec_delta = delta
+        self.prim_len = prim_len
+        self.sec_len = sec_len
+
+    def get_point(prim_steps, sec_steps):
+        return (self.origo
+                + self.prim_delta * prim_steps
+                + self.sec_delta * sec_steps)
+
+
+def transform3(rail):
+    def smaller(left, right):
+        for sec in range(max(left.secondary_size, left.secondary_size)):
+            for pri in range(max(right.primary_size, right.primary_size)):
+                left_point = left.get_point(pri, sec)
+                right_point = right.get_point(pri, sec)
+                if left_point in rail and right_point not in rail:
+                    return True
+                if left_point not in rail and right_point in rail:
+                    return False
+
+    realmax = 0
+    imagmax = 0
+    for point in rail:
+        if point.real > realmax:
+            realmax = point.real
+        if point.imag > imagmax:
+            imagmax = point.imag
+
+    # define the transforms, as where their new origo will be
+    # and along what axis - and in what direction, their new
+    # primary axis would be.
+    transforms = [
+        Transform(complex(0,0),
+                  complex(1,0), complex(0,1), realmax, imagmax),
+        Transform(complex(0,0),
+                  complex(0,1), complex(1,0), imagmax, realmax),
+
+        Transform(complex(0,imagmax),
+                  complex(1,0), complex(0,-1), realmax, imagmax),
+        Transform(complex(0,imagmax),
+                  complex(0,-1), complex(1,0), imagmax, realmax),
+
+        Transform(complex(realmax, 0),
+                  complex(-1,0), complex(0,1), realmax, imagmax),
+        Transform(complex(realmax,0),
+                  complex(0,1), complex(-1,0), imagmax, realmax),
+
+        Transform(complex(realmax,imagmax),
+                  complex(-1,0), complex(0,-1), realmax, imagmax),
+        Transform(complex(realmax,imagmax),
+                  complex(0,-1), complex(-1,0), imagmax, realmax),
+    ]
+
+    smallest = transforms[0]
+    for transform in transforms:
+        if smaller(smallest, transform):
+            smallest = transform
+
+    return apply_transform(transform)
+
+
+    #transforms = []
+    #for origo_x,delta_x in zip((0, realmax), (complex(0,1),complex(0,-1))):
+    #    for origo_y,delta_y in zip((0, imagmax), complex(1,0),complex(-1,0)):
+    #        transforms.append(
+    #            Transform(complex(origo_x, origo_y), delta_x, delta_y, realmax, imagmax)
+    #        )
+    #        transforms.append(
+    #            Transform(complex(origo_x, origo_y), delta_y, delta_x, imagmax, realmax)
+    #        )
+
+
 
 def shift_to_origo(rail):
     min_x = None
@@ -71,33 +154,34 @@ def smallest_nimber_not_in(nimbers):
     return i
 
 
-#TODO optimize
 def split(rail):
     split_rails = []
     remaining = set(rail)
 
     while remaining:
-        split_rails.append({remaining.pop()})
+        split_rails.append([])
+        unchecked_points = [remaining.pop()]
+        while unchecked_points and remaining:
+            check = unchecked_points.pop()
+            for offset in (complex(0,1),
+                           complex(1,0),
+                           complex(0, -1),
+                           complex(-1, 0)
+                          ):
+                k = check+offset
+                if k in remaining:
+                    unchecked_points.append(k)
+                    remaining.remove(k)
+            split_rails[-1].append(check)
+        if unchecked_points:
+            split_rails[-1].extend(unchecked_points)
 
-        while True:
-            added = []
-            for rem in remaining:
-                for offset in (complex(0,1), complex(1,0),
-                               complex(0, -1), complex(-1, 0)):
-                    if offset+rem in split_rails[-1]:
-                        added.append(rem)
-                        break
-            if not added:
-                break
-
-            for add in added:
-                remaining.remove(add)
-                split_rails[-1].add(add)
-
-
-    res = [tuple(sorted(split, key=complex_compare)) for split in split_rails]
-
+    #res = [tuple(sorted(split, key=complex_compare)) for split in split_rails]
+    res = [tuple(split) for split in split_rails]
     return res
+    #return [tuple(r) for r in split_rails]
+
+
 
 mods = (
     (),
@@ -109,19 +193,26 @@ mods = (
 
 
 # removal
-def nimber(rail):
+def nimber(ograil):
     # generate all mirrors & rotations (offset towards zero!)
     # sort each transform individually
     # take the smallest transform
 
+    #if ograil in untransformed_results:
+        #return untransformed_results[ograil]
+    if ograil in results:
+        return results[ograil]
+
     #rail = shift_to_origo(rail)
-    rail = transform(rail)
+    rail = transform(ograil)
 
     # check if we already have a nimber
     #print(rail)
     if rail in results:
-        return results[rail]
-
+        res = results[rail]
+        #if ograil not in untransformed_results:
+            #untransformed_results[ograil] = res
+        return res
 
 
 
@@ -185,31 +276,6 @@ def nimber(rail):
 
 
 
-            #for mod in mods:
-
-            #    remove = [complex(row,col) + m for m in mod]
-            #    valid = True
-
-            #    for r in remove:
-            #        if r not in rail:
-            #            valid = False
-            #            break
-            #    if not valid:
-            #        continue
-
-            #    curr_rail = list(rail)
-            #    curr_rail.remove(complex(row,col))
-
-            #    for r in remove:
-            #        index = curr_rail.index(r)
-            #        curr_rail = curr_rail[:index] + curr_rail[index+1:]
-
-            #    do_stuff(rail, curr_rail, remove)
-
-
-
-
-
     res = smallest_nimber_not_in(global_nimbers)
 
     results[rail] = res
@@ -220,38 +286,67 @@ def nimber(rail):
 
 def do_stuff(rail, curr_rail, remove):
     split_rails = split(curr_rail)
+    #if split_rails != split2(curr_rail):
+        #print(curr_rail)
 
     local_nimber = 0
     for split_rail in split_rails:
         n = nimber(split_rail)
         local_nimber ^= n
 
-    if local_nimber == 0 and rail not in optimal_moves:
-        optimal_moves[rail] = remove
+    if rail not in optimal_moves:
+        optimal_moves[rail] = {local_nimber: remove}
+    elif local_nimber not in optimal_moves[rail]:
+        optimal_moves[rail][local_nimber] = remove
+    return local_nimber
 
+def pickleload(filename):
+    if os.path.isfile(filename):
+        with open(filename, 'rb') as f:
+            return pickle.load(f)
 
+def pickledump(filename, data):
+    with open(filename, 'wb') as f:
+        pickle.dump(data, f)
+
+def generate_block(rows, cols):
+    return tuple(complex(x,y) for x in range(rows)
+              for y in range(cols))
 
 if __name__ == '__main__':
-    #if os.path.isfile('results.pickle'):
-    #    with open('results.pickle', 'rb') as f:
-    #        results = pickle.load(f)
-    #if os.path.isfile('optimal_moves.pickle'):
-    #    with open('optimal_moves.pickle', 'rb') as f:
-    #        optimal_moves = pickle.load(f)
+    parser = argparse.ArgumentParser(description='ALL THE NIMBERS')
+    parser.add_argument('rows', type=int)
+    parser.add_argument('cols', type=int)
+    parser.add_argument('--printres', action='store_true')
+    parser.add_argument('--noload', action='store_true')
+    parser.add_argument('--nodump', action='store_true')
+    args = vars(parser.parse_args())
 
-    #print( nimber(((0,0),(0,1)) ))
-    #print( split(((0,0),(0,1),(2,2)) ))
+    if not args['noload']:
+        results = pickleload('results.pickle')
+        optimal_moves = pickleload('optimal_moves.pickle')
+        #untransformed_results = pickleload('untransformed_results.pickle')
+        #if os.path.isfile('results.pickle'):
+        #    with open('results.pickle', 'rb') as f:
+        #        results = pickle.load(f)
+        #if os.path.isfile('optimal_moves.pickle'):
+        #    with open('optimal_moves.pickle', 'rb') as f:
+        #        optimal_moves = pickle.load(f)
 
-    k = tuple(complex(x,y) for x in range(4) for y in range(4))
+    #k = tuple(complex(x,y) for x in range(args['rows'])
+    #          for y in range(args['cols']))
+    k = generate_block(args['rows'], args['cols'])
     print(nimber(k))
     print(len(results))
-    #import pprint
-    #pprint.pprint(results)
+    #print(len(untransformed_results))
+    if args['printres']:
+        import pprint
+        pprint.pprint(results)
 
-    with open('results.pickle', 'wb') as f:
-        pickle.dump(results, f)
-    with open('optimal_moves.pickle', 'wb') as f:
-        pickle.dump(optimal_moves, f)
+    if not args['nodump']:
+        pickledump('results.pickle', results)
+        pickledump('optimal_moves.pickle', optimal_moves)
+        #pickledump('untransformed_results.pickle', untransformed_results)
 
 
 
